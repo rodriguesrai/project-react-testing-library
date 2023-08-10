500ce853d090ce1dd342a3d15e397312d1599083
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-08-10 20:05:06 -0300

    req 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verifica a página NotFound', () => {
+  it('Verifica se a página contém um headind h2', () => {
+    renderWithRouter(<App />, { route: '*' });
+    const pageTitle = screen.getByRole('heading', { name: /Page requested not found/i, level: 2 });
+    expect(pageTitle).toBeInTheDocument();
+    const imgNotFound = screen.getByRole('img');
+    expect(imgNotFound).toHaveAttribute('alt', "Clefairy pushing buttons randomly with text I have no idea what i'm doing");
+  });
+});
