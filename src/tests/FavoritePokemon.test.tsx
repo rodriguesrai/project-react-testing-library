76388ee059dd8f135165f31a7dec4eaddc846ab4
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-08-10 19:40:35 -0300

    avaliador on?

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,40 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa a página de pokemons favoritos', () => {
+  renderWithRouter(<App />, { route: '/favorites' });
+
+  it('Verifica o título na tela quando não tem pokemons favoritos', () => {
+    const takeTitle = screen.getByText('No favorite Pokémon found');
+    expect(takeTitle).toBeInTheDocument();
+  });
+
+  it('Testa se o pokemon favoritado está na página de favoritos', async () => {
+    renderWithRouter(<App />, { route: '/' });
+
+    const detailsBtn = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    await userEvent.click(detailsBtn);
+
+    const btnFavorite = screen.getByRole('checkbox', {
+      name: /pokémon favoritado/i,
+    });
+    await userEvent.click(btnFavorite);
+
+    const btnFavoritePokemon = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+    await userEvent.click(btnFavoritePokemon);
+
+    // Verifica se o título da página de favoritos está correto
+    // const pageTitle = ////////////////
+    // expect(pageTitle).toBeInTheDocument();
+
+    // Verifica se os pokémons favoritos estão sendo exibidos
+    const pokemonOverview = screen.getByTestId('pokemon-overview');
+    expect(pokemonOverview).toBeInTheDocument();
+  });
+});
