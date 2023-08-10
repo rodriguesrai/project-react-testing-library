ea068c232aa5c553886bf58d69e633073a11d3b1
rodriguesrai <rairodrigues_sf@hotmail.com>
2023-08-10 16:18:26 -0300

    req 1 fix/req 2

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -3,19 +3,8 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-describe('Testando componente App', () => {
-  it('Testando botão home', async () => {
-    renderWithRouter(<App />);
-    const btnHome = screen.getByRole('link', { name: /home/i });
-    expect(btnHome).toBeInTheDocument();
-
-    await userEvent.click(btnHome);
-    const pageTitle = screen.getByRole('heading', { name: /encountered pokémon/i, level: 2 });
-    expect(pageTitle).toBeInTheDocument();
-    // expect(window.location.pathname).toBe('/');
-  });
-
-  it('Testando botão About', async () => {
+describe('Testando página About', () => {
+  it('Verificando se está na página correta', async () => {
     renderWithRouter(<App />);
     const btnAbout = screen.getByRole('link', { name: /about/i });
 
@@ -25,17 +14,5 @@ describe('Testando componente App', () => {
     const img = screen.getByAltText('Pokédex');
     expect(pageTitle).toBeInTheDocument();
     expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-    // expect(window.location.pathname).toBe('/about');
-  });
-
-  it('Testando botão Favorites', async () => {
-    renderWithRouter(<App />);
-    const btnFavorite = screen.getByRole('link', { name: 'Favorite Pokémon' });
-
-    expect(btnFavorite).toBeInTheDocument();
-    await userEvent.click(btnFavorite);
-    const pageTitle = screen.getByRole('heading', { name: /favorite pokémon/i, level: 2 });
-    expect(pageTitle).toBeInTheDocument();
-    // expect(window.location.pathname).toBe('/favorites');
   });
 });
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,1 +1,38 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando componente App', () => {
+  it('Testando botão home', async () => {
+    renderWithRouter(<App />);
+    const btnHome = screen.getByRole('link', { name: /home/i });
+    expect(btnHome).toBeInTheDocument();
+
+    await userEvent.click(btnHome);
+    const pageTitle = screen.getByRole('heading', { name: /encountered pokémon/i, level: 2 });
+    expect(pageTitle).toBeInTheDocument();
+  });
+
+  it('Testando botão About', async () => {
+    renderWithRouter(<App />);
+    const btnAbout = screen.getByRole('link', { name: /about/i });
+
+    expect(btnAbout).toBeInTheDocument();
+    await userEvent.click(btnAbout);
+    const pageTitle = screen.getByRole('heading', { name: /about pokédex/i, level: 2 });
+    const img = screen.getByAltText('Pokédex');
+    expect(pageTitle).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+
+  it('Testando botão Favorites', async () => {
+    renderWithRouter(<App />);
+    const btnFavorite = screen.getByRole('link', { name: 'Favorite Pokémon' });
+
+    expect(btnFavorite).toBeInTheDocument();
+    await userEvent.click(btnFavorite);
+    const pageTitle = screen.getByRole('heading', { name: /favorite pokémon/i, level: 2 });
+    expect(pageTitle).toBeInTheDocument();
+  });
+});
